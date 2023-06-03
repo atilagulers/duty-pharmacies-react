0f24d782be3f59a6b571b83922e4da596a2a1b40
AtilaGuler <108232136+AtiGuler@users.noreply.github.com>
2023-06-03 14:54:20 +0300

    -

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import GoogleMapReact from 'google-map-react';
-import axios from 'axios';
 
 const Marker = ({text, onClick}) => (
   <div className="marker" onClick={onClick}>
@@ -9,32 +8,13 @@ const Marker = ({text, onClick}) => (
 );
 
 const GoogleMap = ({userLocation, selectedPharmacy}) => {
-  const [nearestPharmacy, setNearestPharmacy] = useState(null);
-
   useEffect(() => {
-    const findNearestPharmacy = async () => {
-      //try {
-      //  console.log(selectedPharmacy);
-      //  const response = await axios.get(
-      //    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${selectedPharmacy.latitude},${selectedPharmacy.longitude}&radius=1000&type=pharmacy&key=AIzaSyBsbjUi2u8qPgzs3zY2H0aF6TAG-xJeLCI`
-      //  );
-      //  const nearest = response.data.results[0];
-      //  setNearestPharmacy(nearest);
-      //} catch (error) {
-      //  console.error('Error finding nearest pharmacy:', error);
-      //}
-    };
-    handleMarkerClick();
-    findNearestPharmacy();
-  }, [selectedPharmacy]);
-
-  const handleMarkerClick = () => {
     const replacedText = selectedPharmacy.EczaneAdi.replace(/\s/g, '+');
     window.open(
       `https://www.google.com.tr/search?hl=tr-tr&cs=1&output=search&q=${replacedText}`,
       '_blank'
     );
-  };
+  }, [selectedPharmacy]);
 
   return (
     <div style={{height: '400px', width: '100%'}}>
@@ -48,15 +28,7 @@ const GoogleMap = ({userLocation, selectedPharmacy}) => {
             lat: selectedPharmacy.latitude,
             lng: selectedPharmacy.longitude,
           }}
-        >
-          <Marker
-            position={{
-              lat: selectedPharmacy.latitude,
-              lng: selectedPharmacy.longitude,
-            }}
-            onClick={handleMarkerClick}
-          />
-        </GoogleMapReact>
+        ></GoogleMapReact>
       )}
     </div>
   );
